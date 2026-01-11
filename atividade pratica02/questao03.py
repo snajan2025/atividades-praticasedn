e76ce2ff42971636832025f981b2e9a8a9121343
snajan2025 <betinhonunes2024@gmail.com>
2026-01-11 11:52:34 -0300

    Atividade Prática02-questao03

--- atividade pratica02/questao03.py
+++ atividade pratica02/questao03.py
@@ -1,4 +1,4 @@
-# Atividade Prática02
+# Atividade Prática02-questão03
 nota1 = float(input("Digite a nota 1: "))
 nota2 = float(input("Digite a nota 2: "))
 nota3 = float(input("Digite a nota 3: "))
