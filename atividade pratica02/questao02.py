3e547802d97b4e3d746637bb841c7b2448c4e654
snajan2025 <betinhonunes2024@gmail.com>
2026-01-11 10:50:57 -0300

    Atividade Prática 02 - questão 02

--- atividade pratica02/questao02.py
+++ atividade pratica02/questao02.py
@@ -1,4 +1,12 @@
 nome_produto = input("Digite o nome do produto: ")
-valor = float(input("Digite o preço unitario do produto: "))
-desonto = 20
+valor = float(input("Digite o preço unitário do produto: "))
+desconto_percentual = 20
+
+valor_desconto = (valor * desconto_percentual) / 100
+preco_final = valor - valor_desconto
+
+print(f"Produto: {nome_produto}")
+print(f"Preço original: R$ {valor:.2f}")
+print(f"Valor do desconto: R$ {valor_desconto:.2f}")
+print(f"Preço final: R$ {preco_final:.2f}")
 
