dc162b406592e09d6295ae5c130921907a15f3b6
snajan2025 <betinhonunes2024@gmail.com>
2026-01-12 22:10:07 -0300

    Atividade Prática03-questao01

---
+++ atividadepratica03/questao01.py
@@ -0,0 +1,15 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade < 0:
+    print("Idade invalida:")
+elif idade <= 12:
+    print("Criança")
+elif idade <= 17:
+    print("Adolescente")
+elif idade <= 59:
+    print("Adulto")
+else:
+    print("Idoso")
+
+
+
