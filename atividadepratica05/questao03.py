ae5d28a7b3bd2bed78693cec7cf16641f382f2f2
snajan2025 <betinhonunes2024@gmail.com>
2026-01-15 20:50:31 -0300

    Atividade Prática05-questao03

---
+++ atividadepratica05/questao03.py
@@ -0,0 +1,12 @@
+def calcular_desconto(valor_produto, porcentagem_desconto):
+    
+    desconto = valor_produto * (porcentagem_desconto / 100)
+    valor_final = valor_produto - desconto
+    return valor_final
+
+valor = float(input("Digite o valor do produto: "))
+porcentagem = float(input("Digite a porcentagem de desconto: "))
+
+resultado = calcular_desconto(valor, porcentagem)
+
+print(f"O valor com desconto é de {resultado:.2f}")
