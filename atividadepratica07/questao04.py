130b289de85d27b7e8153eea9a0a890163a67a2b
snajan2025 <betinhonunes2024@gmail.com>
2026-01-22 17:43:55 -0300

    Atividade prática07-questao04

---
+++ atividadepratica07/questao04.py
@@ -0,0 +1,31 @@
+import json
+
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding ='utf-8') as arquivo_json:
+            dados_json = json.load(arquivo_json)
+            return dados_json
+            
+    except FileNotFoundError:
+        print(f"Arquivo {nome_arquivo} não encontrado.")
+
+
+def escrever_arquivo(nome_arquivo, dados):
+    try:
+        with open(nome_arquivo, 'w', encoding ='utf-8') as arquivo_json:
+            json.dump(dados, arquivo_json, indent=4, ensure_ascii=False)
+            return f"Dados escrito com sucesso no arquivo {nome_arquivo}"
+    except Exception as e:
+        return f"Erro ao escrever no arquivo: {e}"
+
+dados = {
+    "nome": "Ana",
+    "idade": 28,
+    "cidade": "São Paulo"
+}
+
+nome = input("Digite o nome do arquivo JSON: ")
+print(escrever_arquivo(nome, dados))
+
+print(ler_arquivo(nome))
+            
