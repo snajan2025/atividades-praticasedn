d2202eae4d3ecf8773b989600aca80ac60d8f89a
snajan2025 <betinhonunes2024@gmail.com>
2026-01-22 17:01:09 -0300

    Atividade prática07-questao03

---
+++ atividadepratica07/questao03.py
@@ -0,0 +1,16 @@
+import csv
+
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', newline = '', encoding ='utf-8') as arquivo_csv:
+            leitor = csv.reader(arquivo_csv)
+            for linha in leitor:
+                print(linha)
+
+    except FileNotFoundError:
+        print(f"Arquivo {nome_arquivo} não encontrado.")
+
+
+nome_arquivo = input("Digite o nome do arquivo CSV: ")
+ler_arquivo(nome_arquivo)
+            
